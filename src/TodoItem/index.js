e2a5dedbfaea6a08c2e8c733f4d09aaea0af9a29
Andres Linan <andreseduardo21010@gmail.com>
2022-06-22 14:42:08 -0500

    delete item correction

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -17,7 +17,7 @@ function TodoItem(props) {
             className="Icon Icon-delete"
             onClick={props.onDelete}
             >
-                
+             X   
             </span>
         </li>
     )
